bda8cf9fab395abfdbca7be0d8e7eebce5a7d7f3
Unknown <brandonmino@bellsouth.net>
2018-06-03 20:40:26 -0400

    Fix Thread Pool Memory Leak

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,12 +1,9 @@
-const os = require('os');
-const version = require('../../package.json').version;
 const threads = require('threads');
 threads.config.set({
     basepath: {
         node: __dirname
     }
 });
-const pool = new threads.Pool();
 const MarketCache = require('./MarketCache');
 const ArbDisplay = require('./ArbDisplay');
 const BinanceApi = require('./BinanceApi');
@@ -38,65 +35,42 @@ BinanceApi.exchangeInfo().then((data) => {
     // Listen for depth updates
     BinanceApi.listenForDepthCache(MarketCache.getTickerArray(), (ticker, depth) => {}, CONFIG.DEPTH_SIZE);
 
+    // Delay before beginning calculation cycle
     console.log(`\nWaiting ${CACHE_INIT_DELAY / 1000} seconds to populate market caches`);
-
-    setTimeout(function() {
-        console.log(`\nInitiated calculation cycle:
-            App Version:     ${version}
-            CPU Cores:       ${os.cpus().length}
-            Cycle Delay:     ${CONFIG.SCAN_DELAY / 1000} seconds
-            Relationships:   ${relationships.length}
-            Investments:     [${CONFIG.INVESTMENT.MIN} - ${CONFIG.INVESTMENT.MAX}] by ${CONFIG.INVESTMENT.STEP} ${CONFIG.BASE_SYMBOL}
-            Profit Logging:  > ${CONFIG.MIN_PROFIT_PERCENT}%\n`);
-        calculateArbitrage();
-    }, CACHE_INIT_DELAY);
+    setTimeout(calculateArbitrage, CACHE_INIT_DELAY);
 })
-    .catch((error) => {
-        console.error(error);
-        console.log(error.message);
-    });
+    .catch(console.error);
 
-// Setup Pool
-let before = new Date();
-let remaining = 0;
 
-pool
-    .run('CalculationNode.js')
-    .on('done', (job, calculated) => {
-        remaining--;
-        if (calculated) {
-            let id = job.sendArgs[0].trade.id;
-            MarketCache.arbs[id] = calculated;
-            //if (calculated.percent >= CONFIG.MIN_PROFIT_PERCENT) console.log(`${new Date()}: Profit of ${calculated.percent.toFixed(5)}% on ${id}`);
-        }
-        if (remaining === 0) {
-            //console.log(`Completed calculations in ${(new Date() - before)/1000} seconds`);
-            displayTopArbs();
-            setTimeout(calculateArbitrage, CONFIG.SCAN_DELAY);
-        }
-    })
-    .on('error', (job, error) => {
-        console.error(error);
-    });
 
 function calculateArbitrage() {
     MarketCache.pruneDepthsAboveThreshold(CONFIG.DEPTH_SIZE);
 
-    remaining = relationships.length;
-    before = new Date();
+    const pool = new threads.Pool();
+    const job = pool
+        .run('CalculationNode.js')
+        .on('error',  console.error)
+        .on('done', (job, calculated) => {
+            if (calculated) {
+                let id = calculated.trade.id;
+                MarketCache.arbs[id] = calculated;
+            }
+        });
 
     relationships.forEach(relationship => {
-        pool.send({
+        job.send({
             trade: relationship,
             minInvestment: CONFIG.INVESTMENT.MIN,
             maxInvestment: CONFIG.INVESTMENT.MAX,
             stepSize: CONFIG.INVESTMENT.STEP,
             MarketCache: MarketCache.getSubsetFromTickers([relationship.ab.ticker, relationship.bc.ticker, relationship.ca.ticker])
-        });
+        })
     });
-}
 
-function displayTopArbs() {
-    let arbsToDisplay = MarketCache.getArbsAboveProfitPercent(CONFIG.MIN_PROFIT_PERCENT);
-    ArbDisplay.displayArbs(arbsToDisplay);
+    pool.on('finished', () => {
+        pool.killAll();
+        let arbsToDisplay = MarketCache.getArbsAboveProfitPercent(CONFIG.MIN_PROFIT_PERCENT);
+        ArbDisplay.displayArbs(arbsToDisplay);
+        setTimeout(calculateArbitrage, CONFIG.SCAN_DELAY);
+    });
 }
